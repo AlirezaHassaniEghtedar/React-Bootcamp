c2de9f92c20f42fc2d3af968efa4b099c3ec534d
Alireza Hassani Eghtedar <alirezaprogrammer2001@gmail.com>
2025-03-05 02:39:03 +0330

    ADD => task-2 | problem-2 | count-number-elements

--- hello-typescript/src/main.ts
+++ hello-typescript/src/main.ts
@@ -1,4 +1,5 @@
 import {reverseArray} from "./task-2/reverse-array";
+import {countNumberElements} from "./task-2/count-number-elements";
 
 function testReverseArray() : void {
     const items = [true , 20 , undefined , null , "Alireza" , 50]
@@ -6,8 +7,15 @@ function testReverseArray() : void {
     console.log(result)
 }
 
+function testCountNumberElements() : void {
+    const items = [true , 20 , undefined , null , "Alireza" , 50];
+    const result = countNumberElements(items);
+    console.log(result)
+}
+
 function main () : void {
     testReverseArray();
+    testCountNumberElements();
 }
 
 main();
