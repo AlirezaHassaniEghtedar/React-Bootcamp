819a120b3da18f1398ed1f50a683238ada2479de
Alireza Hassani Eghtedar <alirezaprogrammer2001@gmail.com>
2025-07-04 23:23:48 +0330

    [ADD] => mock api

--- locana/src/providers/AttractionsProvider.tsx
+++ locana/src/providers/AttractionsProvider.tsx
@@ -1,36 +1,31 @@
-import {PropsWithChildren, ReactNode, useContext, useEffect, useMemo, useState} from "react";
+import {PropsWithChildren, ReactNode, useContext, useEffect, useState} from "react";
 
 import {AttractionsContext} from "../context/attractions-context.ts";
 import {FiltersContext} from "../context/filters-context.ts";
 
 import {Attraction} from "../types/attraction.ts";
 
+import {filterAttractions} from "../../api/attractions.ts";
+
 type Props = PropsWithChildren;
 
 function AttractionsProvider({children}: Props): ReactNode {
     const  {filters} = useContext(FiltersContext)
 
-    const [allAttractions, setAllAttractions] = useState<Attraction[]>([])
-
-    const filteredAttractions = useMemo(() => {
-        return allAttractions.filter((attraction) => {
-            if (filters.tags.length === 0) {
-                return true;
-            }
-
-            return filters.tags.every(tag => attraction.tags.find(x => x.id === tag.id))
-        })
-    }, [filters, allAttractions])
+    const [filteredAttractions, setfilteredAttractions] = useState<Attraction[]>([])
 
     useEffect(() => {
         const fetchAttractions = async (): Promise<void> => {
-            const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/attraction`);
-            const data = await response.json();
-            setAllAttractions(data);
+            // const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/attraction`);
+            // const data = await response.json();
+
+            const data = await filterAttractions(filters)
+
+            setfilteredAttractions(data);
         }
 
         fetchAttractions().then();
-    }, []);
+    }, [filters]);
 
     return <AttractionsContext.Provider value={{filteredAttractions}}>
         {children}
