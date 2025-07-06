e43f4d7f5b98c6b6527bd3830e5470ad95af6c5b
Alireza Hassani Eghtedar <alirezaprogrammer2001@gmail.com>
2025-07-06 23:29:01 +0330

    [ADD] => carousel functionality

--- locana/src/pages/Attraction/components/Carousel/Carousel.tsx
+++ locana/src/pages/Attraction/components/Carousel/Carousel.tsx
@@ -1,15 +1,40 @@
-import {ReactNode} from "react";
+import {ReactNode, useEffect, useState} from "react";
+
+import {Attraction} from "../../../../types/attraction.ts";
 
 import styles from "./Carousel.module.css";
-import {Attraction} from "../../../../types/attraction.ts";
+import clsx from "clsx";
 
 type Props = {
     attraction: Attraction;
 }
 
 function Carousel({attraction}: Props): ReactNode {
+    const [slideIndex, setSlideIndex] = useState<number>(0)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setSlideIndex(old => (old + 1) % attraction.carousel.length)
+        }, 2500)
+
+        return () => {
+            clearInterval(interval)
+        }
+    }, [attraction]);
+
     return <div className={styles['carousel']}>
-        <img src={`${import.meta.env.VITE_CDN_BASE_URL}/${attraction.carousel[0]}`} alt="" height={480}/>
+        <ul style={{transform: `translateX(${slideIndex * 100}%)`}}>
+            {attraction.carousel.map(image => (
+                <li key={image}>
+                    <img src={`${import.meta.env.VITE_CDN_BASE_URL}/${image}`} alt="" height={480}/>
+                </li>
+            ))}
+        </ul>
+        <div className={styles.bullets}>
+            {attraction.carousel.map((_, index) => (
+                <button key={index} className={clsx(styles.bullet, index === slideIndex && styles.active)} onClick={() => setSlideIndex(index)}/>
+            ))}
+        </div>
     </div>
 }
 
