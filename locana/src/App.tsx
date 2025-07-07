09b95f8a4df84ac5de9685836c1800d052969789
Alireza Hassani Eghtedar <alirezaprogrammer2001@gmail.com>
2025-07-07 10:51:14 +0330

    [ADD] => react query

--- locana/src/App.tsx
+++ locana/src/App.tsx
@@ -9,17 +9,23 @@ import About from "./pages/About/About.tsx";
 import NotFound from "./pages/NotFound/NotFound.tsx";
 import Attraction from "./pages/Attraction/Attraction.tsx";
 
+import QueryProvider from "./providers/QueryProvider.tsx";
+
 import "./App.css";
 
 function App(): ReactNode {
-    return <Routes>
-        <Route element={<RootLayout/>}>
-            <Route index element={<Home/>}/>
-            <Route path="attraction/:id" element={<Attraction />} />
-            <Route path="about" element={<About/>}/>
-            <Route path="*" element={<NotFound />}/>
-        </Route>
-    </Routes>
+    return (
+        <QueryProvider>
+            <Routes>
+                <Route element={<RootLayout/>}>
+                    <Route index element={<Home/>}/>
+                    <Route path="attraction/:id" element={<Attraction/>}/>
+                    <Route path="about" element={<About/>}/>
+                    <Route path="*" element={<NotFound/>}/>
+                </Route>
+            </Routes>
+        </QueryProvider>
+    )
 }
 
 export default App;
--- locana/src/providers/AttractionsProvider.tsx
+++ locana/src/providers/AttractionsProvider.tsx
@@ -1,32 +1,24 @@
-import {PropsWithChildren, ReactNode, useContext, useEffect, useState} from "react";
+import {PropsWithChildren, ReactNode, useContext} from "react";
 
 import {AttractionsContext} from "../context/attractions-context.ts";
 import {FiltersContext} from "../context/filters-context.ts";
 
-import {Attraction} from "../types/attraction.ts";
+import {useQuery} from "@tanstack/react-query";
+import {fetchAttractions} from "../api/fetch-attractions.ts";
 
 type Props = PropsWithChildren;
 
 function AttractionsProvider({children}: Props): ReactNode {
-    const  {filters} = useContext(FiltersContext)
+    const {filters} = useContext(FiltersContext)
 
-    const [filteredAttractions, setfilteredAttractions] = useState<Attraction[]>([])
+    const {data : attractions} = useQuery({
+        queryKey: ["attractions", filters],
+        queryFn: () => fetchAttractions(filters),
+        initialData: []
+    })
 
-    useEffect(() => {
-        const fetchAttractions = async (): Promise<void> => {
-            const params = new URLSearchParams();
-            filters.tags.forEach(tag => params.append("tag" , tag.id.toString()))
 
-            const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/attraction?${params.toString()}`);
-            const data = await response.json();
-
-            setfilteredAttractions(data);
-        }
-
-        fetchAttractions().then();
-    }, [filters]);
-
-    return <AttractionsContext.Provider value={{filteredAttractions}}>
+    return <AttractionsContext.Provider value={{filteredAttractions: attractions}}>
         {children}
     </AttractionsContext.Provider>
 }
